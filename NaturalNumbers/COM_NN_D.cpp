2f2665c654f0a1628840d5f80ba3e134c2826704
Borl1seTrunov <borl1se021@icloud.com>
2024-11-04 16:53:02 +0300

    new comp_nn_d.cpp

--- NaturalNumbers/COM_NN_D.cpp
+++ NaturalNumbers/COM_NN_D.cpp
@@ -2,6 +2,12 @@
 
 size_t compareNaturalNaturalNumbers(NaturalNumbers firstNumber, NaturalNumbers secondNumber){
 	int resultOfCompare = firstNumber.getSize() - secondNumber.getSize();
-	if (resultOfCompare == 0) return 0;
+	if (resultOfCompare == 0){
+		for (size_t i = 0; i < firstNumber.getSize(); i++){
+			if (firstNumber.getDigit(i) == secondNumber.getDigit(i)) continue;
+			return firstNumber.getDigit(i) > secondNumber.getDigit(i) ? 2 : 1;
+		}
+		return 0;	
+	};
 	return resultOfCompare > 0 ? 2 : 1;
 }
