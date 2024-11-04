320931e3bf465969d3c950a36a7d3833194bfdbc
Borl1seTrunov <borl1se021@icloud.com>
2024-11-04 19:16:39 +0300

    add general files for naturalNumbers and Integer

--- Rationals/Integer.cpp
+++ Rationals/Integer.cpp
@@ -1,5 +1,10 @@
 #include "Integer.h"
 
+Integer::Integer(){
+	this->sign = true;
+	this->size = 0;
+}
+
 Integer::Integer(int number){
 	this->sign = number >= 0 ? true : false;
 	if (number == 0){
@@ -7,6 +12,7 @@ Integer::Integer(int number){
 		this->digits.at(0) = 0;
 		this->size = 1;
 	}
+	if (this->sign == false) number = -number;
 	while (number > 0){
 		this->digits.insert(this->digits.begin(), number % 10);
 		number /= 10;
@@ -32,15 +38,23 @@ void Integer::setSize(size_t newSize){
 	this->size = newSize;
 }
 
+std::string Integer::getStrReference(){
+	std::string result = this->getSign() ? "" : "-";
+	for (size_t digit : this->digits){
+		result += std::to_string(digit);
+	}
+	return result;
+}
+
 std::vector<size_t> Integer::getDigits(){
 	return this->digits;
 }
 
-size_t Integer::getDigit(size_t position){
+std::pair<StatusCode, size_t> Integer::getDigit(size_t position){
 	if (position >= this->size){
-		return 0;
+		return {Fail, 0};
 	}
-	return this->digits.at(position);
+	return {Successfull, this->digits.at(position)};
 }
 
 void Integer::setDigit(size_t digit, size_t position){
@@ -51,7 +65,7 @@ void Integer::setDigit(size_t digit, size_t position){
 void Integer::setDigits(std::vector<size_t> newDigits){
 	this->digits.clear();
 	this->digits = newDigits;
-	this->size = newDigits.size();
+	this->setSize(newDigits.size());
 }
 
 void Integer::setSign(bool newSign){
