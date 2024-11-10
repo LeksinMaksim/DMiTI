d6d880c1ddc795e24706473bf88854771bfbaf01
Лексин Максим <112461634+LeksinMaksim@users.noreply.github.com>
2024-11-10 19:53:32 +0300

    TRANS_Z_Q

---
+++ Rationals/TRANS_Z_Q.cpp
@@ -0,0 +1,6 @@
+#include "TRANS_Z_Q.h"
+
+Rationals TRANS_Z_Q(const Integer& number)
+{
+    return Rationals(number, NaturalNumbers(1));
+}
---
+++ Rationals/TRANS_Z_Q.h
@@ -0,0 +1,9 @@
+#ifndef TRANS_Z_Q_H
+#define TRANS_Z_Q_H
+
+#include "Rationals.h"
+#include "helpModules/Integer/Integer.h"
+
+Rationals TRANS_Z_Q(const Integer& number);
+
+#endif
