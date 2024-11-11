699940d7c824b8a1fb257de40ecabcf2909abdc2
GeorgiiPoliakov1 <georgiipoliakov111@gmail.com>
2024-11-11 22:37:20 +0300

    made ADD_ZZ_Z

---
+++ Integers/ADD_ZZ_Z.h
@@ -0,0 +1,9 @@
+#include "Integer.h"
+#include "ADD_NN_N.h"
+#include "COM_NN_D.h"
+#include "ABS_Z_N.h"
+#include "SUB_NN_N.h"
+#include "NZER_N_B.h"
+#include "MUL_ZM_Z.h"
+#include "NaturalNumbers.h"
+Integer ADD_ZZ_Z(Integer first, Integer second);
