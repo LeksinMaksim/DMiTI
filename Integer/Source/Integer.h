10c42636411d7478542c89dc15daeb1f2e79a31f
Borl1seTrunov <borl1se021@icloud.com>
2024-11-13 03:53:15 +0300

    add Integer general files and add POZ_Z_D function

---
+++ Integer/Source/Integer.h
@@ -0,0 +1,30 @@
+#ifndef INTEGER_HEADER
+#define INTEGER_HEADER
+
+#include <iostream>
+#include <vector>
+#include "./StatusCode.h"
+#include "./SignStatus.h"
+
+class Integer{
+	private:
+		SignStatus sign;
+		size_t size;
+		std::vector<size_t> digits;
+	public:
+		Integer();
+		Integer(int number);
+		Integer(const Integer& otherNumber);
+		SignStatus getSign();
+		size_t getSize();
+		void setSize(size_t newSize);
+		std::string getStrReference();
+		std::vector<size_t> getDigits();
+		std::pair<StatusCode, size_t> getDigit(size_t position);
+		void setDigit(size_t digit, size_t position);
+		void setDigits(std::vector<size_t> newDigits);
+		void setSign(SignStatus newSign);
+		Integer& operator=(const Integer& number);
+};
+
+#endif
