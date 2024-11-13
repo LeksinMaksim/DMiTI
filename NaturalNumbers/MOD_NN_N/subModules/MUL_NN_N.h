b9eabde7904b5fd810efec8ff4e0a2bd4653661f
Лексин Максим <112461634+LeksinMaksim@users.noreply.github.com>
2024-11-13 15:24:21 +0300

    MOD_NN_N

---
+++ NaturalNumbers/MOD_NN_N/subModules/MUL_NN_N.h
@@ -0,0 +1,10 @@
+#ifndef MUL_NN_N_HEADER
+#define MUL_NN_N_HEADER
+
+#include "../../NaturalNumbers.h"
+#include "ADD_NN_N.h"
+#include "MUL_Nk_N.h"
+
+NaturalNumbers MUL_NN_N(NaturalNumbers firstNumber, NaturalNumbers secondNumber);
+
+#endif
