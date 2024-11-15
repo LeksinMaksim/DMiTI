911f7c60faadc61449325360d9fdf144b2a4156c
Alexander-Snigirev <144445249+Alexander-Snigirev@users.noreply.github.com>
2024-11-16 01:15:35 +0300

    Update NaturalNumbers.h

--- NaturalNumbers/NaturalNumbers.h
+++ NaturalNumbers/NaturalNumbers.h
@@ -3,27 +3,26 @@
 
 #include <iostream>
 #include <vector>
-
 #include "StatusCode.h"
 
-class NaturalNumbers
-{
-private:
-    size_t size;
-    std::vector<size_t> digits;
-
-public:
-    NaturalNumbers();
-    NaturalNumbers(size_t number);
-    NaturalNumbers(const NaturalNumbers& otherNumber);
-    std::vector<size_t> getDigits();
-    size_t getSize();
-    void setSize(size_t newSize);
-    std::string getStrReference();
-    void setDigit(size_t Digit, size_t position);
-    void setDigits(std::vector<size_t> newDigits);
-    std::pair<StatusCode, size_t> getDigit(size_t position);
-    NaturalNumbers& operator=(const NaturalNumbers& number);
+class NaturalNumbers{
+	private:
+		size_t size;
+		std::vector<size_t> digits;
+	public:
+		NaturalNumbers();
+		NaturalNumbers(size_t number);
+		NaturalNumbers(const NaturalNumbers& otherNumber);
+		NaturalNumbers(std::string input);
+		std::vector<size_t> getDigits();
+		size_t getSize();
+		void setSize(size_t newSize);
+		std::string getStrReference();
+		void setDigit(size_t Digit, size_t position);
+		void setDigits(std::vector<size_t> newDigits);
+		std::pair<StatusCode, size_t> getDigit(size_t position);
+		NaturalNumbers& operator=(const NaturalNumbers& number);
+		bool NaturalNumbers::operator == (const NaturalNumbers& other);
 };
 
 #endif
