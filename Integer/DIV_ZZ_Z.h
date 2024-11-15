fd9ec37d0a63af3f2fcbeb11bc4f0d1283d001c0
Alexander-Snigirev <144445249+Alexander-Snigirev@users.noreply.github.com>
2024-11-16 01:18:13 +0300

    Update DIV_ZZ_Z.h

--- Integer/DIV_ZZ_Z.h
+++ Integer/DIV_ZZ_Z.h
@@ -2,6 +2,7 @@
 #define DIV_ZZ_Z_H
 #include "Source/Integer.h"
 #include "../NaturalNumbers/DIV_NN_N.h"
+#include "../NaturalNumbers/MUL_NN_N.h"
 #include "../NaturalNumbers/ADD_1N_N.h"
 #include "POZ_Z_D.h"
 //#include "ABS_Z_N.h"
