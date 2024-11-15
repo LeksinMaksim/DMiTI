0f60c220721e4bbdf2e467e20d3d62ce524530ba
Piankov-Michail <mishan.rus2014@gmail.com>
2024-11-15 14:35:10 +0300

    maybe fixed LCM_NN_N

--- NaturalNumbers/LCM_NN_N.cpp
+++ NaturalNumbers/LCM_NN_N.cpp
@@ -4,7 +4,7 @@
 
 NaturalNumbers LCM_NN_N(NaturalNumbers first, NaturalNumbers second)
 {
-    NaturalNumbers NOD = LCM_NN_N(first, second); // Вычисление НОД двух чисел
+    NaturalNumbers NOD = GCF_NN_N(first, second); // Вычисление НОД двух чисел
     NaturalNumbers multiplication = MUL_NN_N(first, second); // Вычисление произведения двух чисел
     return DIV_NN_N(NOD, multiplication); // Деление произведения на НОД по формуле НОК через НОД
 }
