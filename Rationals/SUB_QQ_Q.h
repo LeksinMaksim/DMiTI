e0b7e98e8863e5f4130b69fb4a58101bce35dd76
GeorgiiPoliakov1 <georgiipoliakov111@gmail.com>
2024-11-15 14:07:21 +0300

    made SUB_QQ_Q

---
+++ Rationals/SUB_QQ_Q.h
@@ -0,0 +1,9 @@
+#include "TRANS_N_Z.h"
+#include "DIV_NN_N.h"
+#include "LCM_NN_N.h"
+#include "SUB_ZZ_Z.h"
+#include "MUL_ZZ_Z.h"
+#include "RED_Q_Q.h"
+#include "Rationals.h"
+
+Rationals SUB_QQ_Q(Rationals firstNumber, Rationals secondNumber);
