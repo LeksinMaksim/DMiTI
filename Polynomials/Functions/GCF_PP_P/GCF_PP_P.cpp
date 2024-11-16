a36aaaf5d2f3e56cfad44bb703436a09c81095ec
Piankov-Michail <mishan.rus2014@gmail.com>
2024-11-16 14:15:13 +0300

    dskl

--- Polynomials/Functions/GCF_PP_P/GCF_PP_P.cpp
+++ Polynomials/Functions/GCF_PP_P/GCF_PP_P.cpp
@@ -1,5 +1,13 @@
 #include "GCF_PP_P.h"
 
+Polynomials GCF_PP_P_Interactive()
+{
+    Polynomials poly1 = NumberInput::readPolynomial("Введите первый полином: ");
+    Polynomials poly2 = NumberInput::readPolynomial("Введите второй полином: ");
+    return GCF_PP_P(poly1, poly2);
+}
+
+
 Polynomials GCF_PP_P(Polynomials a, Polynomials b){
     
     while ((a.getSize() != 0 && b.getSize() != 0) && (a.getStrReference().compare(0,1,"0")) && (b.getStrReference().compare(0,1,"0"))){
